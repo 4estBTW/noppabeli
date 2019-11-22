54f93cd87fd74e4c68977f2690d99275bd43dde6
Tatu Turunen <tatu.turunen@itvelhot.local>
2019-11-22 08:38:09 +0200

    uuu jee ;)

--- main.js
+++ main.js
@@ -60,22 +60,3 @@ function endTurn(){
 
 updateUi();
 
-
-
-// function throwdice() {
-//     var randomdice = Math.round(Math.random() * 5)
-//     document.images["mydice"].src = eval("face" + randomdice + ".src")
-// }
-
-// var face0 = new Image()
-// face0.src = "d1.gif"
-// var face1 = new Image()
-// face1.src = "d2.gif"
-// var face2 = new Image()
-// face2.src = "d3.gif"
-// var face3 = new Image()
-// face3.src = "d4.gif"
-// var face4 = new Image()
-// face4.src = "d5.gif"
-// var face5 = new Image()
-// face5.src = "d6.gif"
